cb52464ff5848343418b76bb2eac7b481651466d
Pat Tovo <patricia.tovo@hpe.com>
2021-06-24 14:15:14 -0500

    SAT-796, SAT-847: Updated CHANGELOG and copyright

--- sat-manpage.sh
+++ sat-manpage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Wrapper script for sat man pages
 #
-# (C) Copyright 2020 Hewlett Packard Enterprise Development LP.
+# (C) Copyright 2021 Hewlett Packard Enterprise Development LP.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
